dd1173e4a3b2c4f56d5e1a04a45bfb9f4f2754fb
Rudinilly-ti <rudinilly-ti@outolook.com>
2024-01-08 09:57:52 -0300

    first commit

---
+++ src/auth/users.rspository.ts
@@ -0,0 +1,38 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+  constructor(private datasource: DataSource) {
+    super(User, datasource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw InternalServerErrorException;
+      }
+    }
+  }
+}
